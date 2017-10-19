1249b87d5057780059b1ffebe87ac357bc9b2ea9
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-20 00:21:58 +0900

    最初のコミット。雛形となり得るモノを追加。

---
+++ google-page-register/src/main/java/com/github/kazuhito_m/googlepageregister/Application.java
@@ -0,0 +1,12 @@
+package com.github.kazuhito_m.googlepageregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePageRegisterApplication.class, args);
+	}
+}
