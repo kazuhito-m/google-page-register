c7a3b76b2c73e32c4aad023154c1b6802314d307
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-22 03:35:05 +0900

    実装。WebDriverの各種設定と、Seleniumのサンプルプログラムを追加。

--- google-page-register/src/main/java/com/github/kazuhito_m/googlepageregister/webbrothercontrol/GoogleUrlRegister.java
+++ google-page-register/src/main/java/com/github/kazuhito_m/googlepageregister/webbrothercontrol/GoogleUrlRegister.java
@@ -1,20 +1,46 @@
 package com.github.kazuhito_m.googlepageregister.webbrothercontrol;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
 @Component
+@PropertySource(value = "classpath:application.yml")
 public class GoogleUrlRegister {
-
     private static Logger logger = LoggerFactory.getLogger(GoogleUrlRegister.class);
 
-    public void register(List<URL> links) {
-        logger.info("Googleページ登録も通っているよ");
-        // TODO 以下、仮実装を失くす。
-        links.stream().forEach(System.out::println);
+    @Value("${google.id}")
+    private String googleId;
+
+    @Value("${google.pass}")
+    private String googlePass;
+
+    public void register(List<URL> links) throws IOException {
+        WebDriver driver = new WebDriverSelector().choice();
+
+        // サンプルソース
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!");
+        element.submit();
+        logger.info("Page title1 is: " + driver.getTitle());
+        (new WebDriverWait(driver, 30)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().startsWith("cheese!");
+            }
+        });
+        logger.info("Page title2 is: " + driver.getTitle());
+        driver.quit();
     }
 }
---
+++ google-page-register/src/main/java/com/github/kazuhito_m/googlepageregister/webbrothercontrol/WebDriverSelector.java
@@ -0,0 +1,48 @@
+package com.github.kazuhito_m.googlepageregister.webbrothercontrol;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class WebDriverSelector {
+
+    private static Logger logger = LoggerFactory.getLogger(WebDriverSelector.class);
+
+    public WebDriver choice() throws IOException {
+        URL webDriverUrlInJar = webDriverUrl();
+
+        // Jar内のファイルでは「実行」出来ない。一時ファイルにコピー・実行権限を与える。
+        File tempWebDriverPath = tempWebDriverPath();
+        FileUtils.copyURLToFile(webDriverUrlInJar, tempWebDriverPath);
+        tempWebDriverPath.setExecutable(true);
+        logger.info("Copy WebDriver to executable temp file : " + tempWebDriverPath.getCanonicalPath());
+
+        System.setProperty("webdriver.chrome.driver", tempWebDriverPath.getCanonicalPath());
+        return new ChromeDriver();
+    }
+
+    private URL webDriverUrl() {
+        String envType = environmentName();
+        String extension = "win32".equals(envType) ? ".exe" : "";
+        String webDriverPath = String.format("webdriver/%s/chromedriver%s", envType, extension);
+        URL url = this.getClass().getResource(webDriverPath);
+        return url;
+    }
+
+    private String environmentName() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.startsWith("linux")) return "linux64";
+        if (os.startsWith("mac")) return "mac64";
+        return "win32";
+    }
+
+    private File tempWebDriverPath() throws IOException {
+        return File.createTempFile("webdriver", ".temp");
+    }
+}
